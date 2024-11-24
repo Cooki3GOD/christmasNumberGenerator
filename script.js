849e03a02f51b68f12f49e90a60e07de0a44a368
Łukasz Szczygielski <l_szczygielski@o2.pl>
2024-11-24 17:04:06 +0100

    Ukonczono funkcje generujaca liczby

--- script.js
+++ script.js
@@ -4,15 +4,29 @@ const display = document.querySelector("h2 p");
 const listOfNumbers = document.querySelectorAll("li");
 const button = document.querySelector("button");
 const usedNumbers = [];
-console.log(listOfNumbers);
+const numbers = [];
+
 // Funkcje
+
+for(let i = 0; i < listOfNumbers.length; i++) {
+    numbers.push(parseInt(listOfNumbers[i].textContent, 10));
+}
 
 function getRandomNumber() {
     if(listOfNumbers.length == 0) {
-        display.innerText = "Wszystkie Numery Zostaly wylosowane!";
+        display.innerText = "Nie ma liczb do wylosowania!";
     }
 
     let number;
+    
+    do {
+        let index = Math.floor(Math.random() * numbers.length);
+        number = numbers[index];
+        listOfNumbers[index].remove();
+    }
+    while(usedNumbers.includes(number) && listOfNumbers.length != 0);
+    usedNumbers.push(number);
+    display.innerText  = number;
 }
 
 button.addEventListener("click", getRandomNumber);
